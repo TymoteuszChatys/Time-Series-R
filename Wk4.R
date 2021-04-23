e1431a03a045394e0567668467d2208c582677c5
tymoteuszchatys <tymoteuszchatys@gmail.com>
2021-04-23 15:53:18 +0100

    Wk4 done

--- Wk4.R
+++ Wk4.R
@@ -1,6 +1,12 @@
 
+Box.test(x,k,type="Ljung-Box") #gives a p-value for this test using data in x
+#The test is significant at level alpha if p-value < alpha. Otherwise the test is not significant
 
-acf(x,5,plot=F) #auto-correlation function
-pacf(x,5,plot=F) # partial auto-correlation function
+x <- rnorm(100) #r for random, norm for normal
+plot(x, type="l")
 
+#par(mfrow=c(2,1)) #split graph window into 2
+#acf(x,20,plot=F) #sample acf at lags up to 20
+#pacf(x,20,plot=F) #sample pacf at lags up to 20
 
+Box.test(x,5,type="Ljung-Box")
